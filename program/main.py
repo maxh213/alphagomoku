f1894198cb535e76afa04046f2b31cd98877f02d
Radu Grigore <radugrigore@gmail.com>
2016-09-08 19:31:39 +0100

    small fixexs

--- program/main.py
+++ program/main.py
@@ -1,22 +1,8 @@
 #! /usr/bin/env python3
 import board as brd
+from copy import deepcopy
 from sys import argv
 
-# Char that represents player 1
-CHAR_PLAYER_1 = 'X'
-# Char that represents player 2
-CHAR_PLAYER_2 = 'Y'
-# Char that represents unclaimed territory
-CHAR_NEUTRAL = '-'
-
-def convertPlayerChar(player):
-	if player == -1:
-		return CHAR_PLAYER_1
-	if player == 0:
-		return CHAR_NEUTRAL
-	if player == 1:
-		return CHAR_PLAYER_2
-	return None
 
 def parseTrainingFile(path):
 	with open(path) as f:
@@ -27,22 +13,33 @@ def parseTrainingFile(path):
 	ls = [l.split(',')[:2] for l in ls]
 	return [(int (m[0]) - 1,int (m[1]) - 1) for m in ls]
 
-def simulateBoard(moves, shouldPrint=False):
+def simulate(moves, shouldPrint=False):
 	board = [[0 for j in range(brd.BOARD_SIZE)]for i in range(brd.BOARD_SIZE)]
-	p = 1
+	all_boards = [board]
+	p = -1
 	for x,y in moves:
-		if board[x][y] != 0 and shouldPrint:
-			print ('Warn: board move invalid')
+		assert board[x][y] == 0
 		board[x][y] = p
 		winner = brd.decideWinner(board)
 		if winner != 0:
-			if shouldPrint:
-				print ("Winner found: " + convertPlayerChar(winner))
-			return winner
+			return (winner, all_boards)
 		p = -p
+		all_boards.append(deepcopy(board))
 		if shouldPrint:
 			brd.printBoard(board)
+	assert False
 
-moves = parseTrainingFile(argv[1])
-print(convertPlayerChar(simulateBoard(moves)))
+
+def main():
+	data = []
+	for filename in argv[1:]:
+		print('processing file', filename)
+		moves = parseTrainingFile(filename)
+		winner, boards = simulate(moves, shouldPrint=True)
+		data.extend((b, winner) for b in boards)
+	print(data)
+
+if __name__ == '__main__':
+	main()
+
 
