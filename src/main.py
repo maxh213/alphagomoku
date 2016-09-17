a928117d12b4d2c4640f177be502c96e1b7234fc
Harry Clarke <harry-clarke@outlook.com>
2016-09-17 17:22:16 +0100

    Removed field duplication: Player character codes were represented in both Board, and main. They belong in Board.

--- src/main.py
+++ src/main.py
@@ -4,17 +4,6 @@ import os.path
 from copy import deepcopy
 from sys import argv
 
-CHAR_PLAYER_1 = 'X'
-CHAR_PLAYER_2 = 'Y'
-# Char that represents unclaimed territory
-CHAR_NEUTRAL = '-'
-PLAYER_CODES = [CHAR_PLAYER_1, CHAR_NEUTRAL, CHAR_PLAYER_2]
-
-
-def convert_player_char(player):
-	assert -1 <= player <= 1, "Invalid board cell contents"
-	return PLAYER_CODES[player + 1]
-
 
 def expand_dirs(path, arr=None):
 	if arr is None:
