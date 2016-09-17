22f0aa51fefe3467a51bf93adf41662776f8d574
Harry Clarke <harry-clarke@outlook.com>
2016-09-17 17:20:47 +0100

    Converted to OO version of Board.

--- src/board.py
+++ src/board.py
@@ -1,59 +1,68 @@
 from sys import stdout
 
-BOARD_SIZE = 20
-# The number of pieces that are required to be in a row to win.
-COUNT_NEEDED = 5
+class Board:
 
-CHAR_PLAYER_1 = 'X'
-CHAR_PLAYER_2 = 'Y'
-# Char that represents unclaimed territory
-CHAR_NEUTRAL = '-'
-PLAYER_CODES = [CHAR_PLAYER_1, CHAR_NEUTRAL, CHAR_PLAYER_2]
+	BOARD_SIZE = 20
+	# The number of pieces that are required to be in a row to win.
+	COUNT_NEEDED = 5
+
+	CHAR_PLAYER_1 = 'X'
+	CHAR_PLAYER_2 = 'Y'
+	# Char that represents unclaimed territory
+	CHAR_NEUTRAL = '-'
+	PLAYER_CODES = [CHAR_PLAYER_1, CHAR_NEUTRAL, CHAR_PLAYER_2]
 
 
-"""Access the PLAYER_CODES array to determine which code
-should be used"""
-def convert_player_char(player):
-	assert -1 <= player <= 1, "Invalid board cell contents"
-	return PLAYER_CODES[player + 1]
 
-def print_board(board):
-	for row in board:
-		for coord in row:
-			stdout.write(convert_player_char(coord))
-		print()
-	print()
+	"""Access the PLAYER_CODES array to determine which code
+	should be used"""
 
-def decide_winner_line(board, x, y, dx, dy):
-	# Coords at end of vector.
-	restingX = x + COUNT_NEEDED * dx
-	restingY = y + COUNT_NEEDED * dy
+	def __init__(self):
+		self.board = [[0 for j in range(Board.BOARD_SIZE)] for i in range(Board.BOARD_SIZE)]
+		self.next_player = -1
 
-	# Check line doesn't leave the board from the left or the top.
-	if x < 0 or restingX < 0:
-		return 0
-	if y < 0 or restingY < 0:
-		return 0
-	# Check line doesn't leave the board from the right or the bottom.
-	if x > BOARD_SIZE or restingX > BOARD_SIZE:
-		return 0
-	if y > BOARD_SIZE or restingY > BOARD_SIZE:
-		return 0
+	def _decide_winner_line(self, x, y, dx, dy):
+		# Coords at end of vector.
+		restingX = x + Board.COUNT_NEEDED * dx
+		restingY = y + Board.COUNT_NEEDED * dy
 
-	start = board[x][y]
-	for i in range(COUNT_NEEDED):
-		if board[x][y] != start:
+		# Check line doesn't leave the board from the left or the top.
+		if x < 0 or restingX < 0:
 			return 0
-		x+=dx
-		y+=dy
-	return start
+		if y < 0 or restingY < 0:
+			return 0
+		# Check line doesn't leave the board from the right or the bottom.
+		if x > Board.BOARD_SIZE or restingX > Board.BOARD_SIZE:
+			return 0
+		if y > Board.BOARD_SIZE or restingY > Board.BOARD_SIZE:
+			return 0
 
-def decide_winner (board):
-	for y in range(BOARD_SIZE):
-		for x in range(BOARD_SIZE):
-			for step in [(1,0),(0,1),(1,1),(1,-1)]:
-				winner = decide_winner_line(board, x, y, step[0], step[1])
-				if winner != 0:
-					return winner
-	return 0
+		start = self.board[x][y]
+		for i in range(Board.COUNT_NEEDED):
+			if self.board[x][y] != start:
+				return 0
+			x += dx
+			y += dy
+		return start
+
+	def print_board(self):
+		for row in self.board:
+			for coord in row:
+				stdout.write(Board.convert_player_char(coord))
+			print()
+		print()
+
+	def decide_winner(self):
+		for y in range(Board.BOARD_SIZE):
+			for x in range(Board.BOARD_SIZE):
+				for step in [(1, 0), (0, 1), (1, 1), (1, -1)]:
+					winner = self._decide_winner_line(x, y, step[0], step[1])
+					if winner != 0:
+						return winner
+		return 0
+
+	@staticmethod
+	def convert_player_char(player):
+		assert -1 <= player <= 1, "Invalid board cell contents"
+		return Board.PLAYER_CODES[player + 1]
 
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 import board as brd
+import os.path
 from copy import deepcopy
 from sys import argv
 
@@ -11,46 +12,75 @@ PLAYER_CODES = [CHAR_PLAYER_1, CHAR_NEUTRAL, CHAR_PLAYER_2]
 
 
 def convert_player_char(player):
-    assert -1 <= player <= 1, "Invalid board cell contents"
-    return PLAYER_CODES[player + 1]
+	assert -1 <= player <= 1, "Invalid board cell contents"
+	return PLAYER_CODES[player + 1]
+
+
+def expand_dirs(path, arr=None):
+	if arr is None:
+		arr = []
+	if os.path.isdir(path):
+		for f in os.listdir(path):
+			expand_dirs(path + os.sep + f, arr)
+	elif os.path.isfile(path) and os.path.splitext(path)[1] == '.psq':
+		arr.append(path)
+	return arr
 
 
 def parse_training_file(path):
-    with open(path) as f:
-        ls = f.readlines()
+	with open(path) as f:
+		ls = f.readlines()
 
-    ls = ls[1:-3]
-    ls = [l.strip() for l in ls]
-    ls = [l.split(',')[:2] for l in ls]
-    return [(int(m[0]) - 1, int(m[1]) - 1) for m in ls]
+	ls = ls[1:-3]
+	ls = [l.strip() for l in ls]
+	ls = [l.split(',')[:2] for l in ls]
+	return [(int(m[0]) - 1, int(m[1]) - 1) for m in ls]
 
 
 def simulate(moves, should_print=False):
-    board = [[0 for j in range(brd.BOARD_SIZE)] for i in range(brd.BOARD_SIZE)]
-    all_boards = [board]
-    p = -1
-    for x, y in moves:
-        assert board[x][y] == 0
-        board[x][y] = p
-        all_boards.append(deepcopy(board))
-        if should_print:
-            brd.print_board(board)
-        winner = brd.decide_winner(board)
-        if winner != 0:
-            return (winner, all_boards)
-        p = -p
-    raise ValueError('Winner still not determined after all moves have been made.')
+	board = [[0 for j in range(brd.BOARD_SIZE)] for i in range(brd.BOARD_SIZE)]
+	all_boards = [board]
+	p = -1
+	for x, y in moves:
+		assert board[x][y] == 0
+		board[x][y] = p
+		all_boards.append(deepcopy(board))
+		if should_print:
+			brd.print_board(board)
+		winner = brd.decide_winner(board)
+		if winner != 0:
+			return (winner, all_boards)
+		p = -p
+	raise ValueError('Winner still not determined after all moves have been made.')
+
+
+def simulate(moves, should_print=False):
+	board = brd.Board()
+	all_boards = [board.board]
+	p = -1
+	for x, y in moves:
+		assert board.board[x][y] == 0
+		board.board[x][y] = p
+		all_boards.append(deepcopy(board.board))
+		if should_print:
+			board.print_board()
+		winner = board.decide_winner()
+		if winner != 0:
+			return winner, all_boards
+		p = -p
+	raise ValueError('Winner still not determined after all moves have been made.')
 
 
 def main():
-    data = []
-    for filename in argv[1:]:
-        print('processing file', filename)
-        moves = parse_training_file(filename)
-        winner, boards = simulate(moves, should_print=True)
-        data.extend((b, winner) for b in boards)
-    print(data)
+	data = []
+	for path in argv[1:]:
+		for filename in expand_dirs(path):
+			print('processing file', filename)
+			moves = parse_training_file(filename)
+			winner, boards = simulate(moves, should_print=False)
+			data.extend((b, winner) for b in boards)
+		print(data)
 
 
 if __name__ == '__main__':
-    main()
+	main()
