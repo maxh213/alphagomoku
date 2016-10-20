7e5b1439cd60e1b3437e25029f9371a3cceae834
Matt Clayton <mc661@kent.ac.uk>
2016-10-20 22:10:46 +0100

    Added input valdation to main_gomoku, and fixed a reference to board.board (it is not board._board)
    MCMC
    
    
    Former-commit-id: 6c7f201caed28fd1a42405cea91e114602d0cbc8

--- src/gomoku_app/gomoku.py
+++ src/gomoku_app/gomoku.py
@@ -5,5 +5,5 @@ class Gomoku:
 	board = Board()
 
 	def make_move(self, x, y, player):
-		self.board.board[y][x] = player
+		self.board._board[y][x] = player
 		self.board.print_board()
--- src/gomoku_app/main_gomoku.py
+++ src/gomoku_app/main_gomoku.py
@@ -1,13 +1,25 @@
+import re
+
 from pip._vendor.distlib.compat import raw_input
 
 from gomoku_app.gomoku import Gomoku
+
+
+def validate_input(move):
+	pattern = re.compile('(1[0-9]|[0-9]),(1[0-9]|[0-9])')
+	return pattern.match(move) is not None
+
 
 if __name__ == '__main__':
 	gomoku = Gomoku()
 	player = -1
 	while (1):
 		player = -player
-		move = raw_input("Type your move (X,Y): ")
+		move = None
+		while (1):
+			move = raw_input("Type your move (X,Y): ")
+			if (validate_input(move)):
+				break
 		coordinates = move.split(",")
 		x_coordinate = coordinates[0]
 		y_coordinate = coordinates[1]
