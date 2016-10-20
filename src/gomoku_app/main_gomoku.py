cec177518aac08fc806eeb357410ccbdac304abf
Matt Clayton <mc661@kent.ac.uk>
2016-10-20 21:00:33 +0100

    Added user input to main_gomoku, as well as player switching each turn, and printing after each move.
    MCMC
    
    
    Former-commit-id: 6438940f8fdc97124163abaa8fb01e9db561a528

--- src/gomoku_app/main_gomoku.py
+++ src/gomoku_app/main_gomoku.py
@@ -1,6 +1,14 @@
+from pip._vendor.distlib.compat import raw_input
+
 from gomoku_app.gomoku import Gomoku
 
 if __name__ == '__main__':
 	gomoku = Gomoku()
-	gomoku.make_move(19, 19, -1)
-	gomoku.make_move(19, 18, 1)
+	player = -1
+	while (1):
+		player = -player
+		move = raw_input("Type your move (X,Y): ")
+		coordinates = move.split(",")
+		x_coordinate = coordinates[0]
+		y_coordinate = coordinates[1]
+		gomoku.make_move(int(x_coordinate), int(y_coordinate), player)
