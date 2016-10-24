c7114a25cc17a407520097d443cae802eefba952
Max Harris <mh568@kent.ac.uk>
2016-10-24 20:02:20 +0100

    added better accuracy guaging
    Former-commit-id: e9eb604ed0226f0121b48126eeb3d5766770812d

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-from neural_network import tensorMain
+from test_network import network
 
 #Main file that points to the start of our app
 if __name__ == '__main__':
-        tensorMain()
+        network()
 
 
 
--- src/test_network.py
+++ src/test_network.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 from training_data import process_training_data, get_files, get_test_files
 
+#TODO: this should be got from the board file
 # Width and Height of the board
 BOARD_SIZE = 20
 
@@ -9,6 +10,15 @@ LEARNING_RATE = 0.1
 
 FILE_COUNT = 100
 
+'''
+Training data format:
+trainingData[0] = first game
+trainingData[0][1][0] = first move of first game
+trainingData[0][2][0] = second move of first game
+trainingData[0][0][1] = winner of first game
+trainingData[0][1][0][0] = first line of first move of first game
+trainingData[0][1][0][0][0] = first tile on first line of first move of first game
+''' 
 def get_training_data():
     # Obtain files for processing
     files = get_files()
@@ -46,33 +56,45 @@ def network():
     sess = tf.Session()
     sess.run(init)
 
+
     # For each game
     for i in range (len(training_data)):
         # For each move in the game
         for j in range (len(training_data[i])):
             batch_input = training_data[i][j][0]
-            batch_output = transformTrainingOutputForTF(training_data[i][j][1])
+            batch_output = transform_training_output_for_tf(training_data[i][j][1])
             #print("Game: %s, Move: %s, Winner: %s" % (i + 1, j + 1, batch_output[0][0]))
             sess.run(train_step, feed_dict={input: batch_input, correct: batch_output})
-            
-            #The code below outputs the prediction made by the nn vs the actual training data output
-            print("*******************************")
+
+            print("********************************")
             print("Ouput from network:")
             printableOuput = sess.run(model, feed_dict={input: batch_input, correct: batch_output})
             print(printableOuput[0][0]) 
             print("Output from training data:")
-            print(transformTrainingOutputForTF(training_data[i][j][1])[0][0])
-            print("*******************************")
+            print(transform_training_output_for_tf(training_data[i][j][1])[0][0])
+            print("********************************")
 
     # Determine accuracy
     correct_predicition = tf.equal(tf.argmax(model, 1), tf.argmax(correct, 1))
 
     accuracy = tf.reduce_mean(tf.cast(correct_predicition, tf.float32))
+    
+    accuracy_list = []
+     # For each game
+    for i in range (0, len(testing_data)):
+        # For each move in the game
+        for j in range (0, len(testing_data[i])):
+            batch_input = testing_data[i][j][0]
+            batch_output = transform_training_output_for_tf(testing_data[i][j][1])
+            printableOuput = sess.run(model, feed_dict={input: batch_input, correct: batch_output})
+            accuracy_list.append([testing_data[i][j][1], printableOuput[0][0], sess.run(accuracy, feed_dict={input: testing_data[i][j][0], correct: transform_training_output_for_tf(testing_data[i][j][1])})])
+            
+    #total_accuracy = sum(accuracy_list) / float(len(accuracy_list))
+    print(accuracy_list)
+    #print("Neural Network Accuracy: ", total_accuracy)
 
-    print(sess.run(accuracy, feed_dict={input: testing_data[10][0][0], correct: transformTrainingOutputForTF(testing_data[10][0][1])}))
 
-
-def transformTrainingOutputForTF(actualTrainingOutput):
+def transform_training_output_for_tf(actualTrainingOutput):
 	return [
 	    [actualTrainingOutput, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
