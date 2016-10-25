3de054917b7adf3c98994f091094ef8ab3f52654
Max Harris <mh568@kent.ac.uk>
2016-10-25 21:08:34 +0100

    added conversion from -1 to 0
    Former-commit-id: aef5419eef0b06de7cb3cc30e9e9e7decd1e65ab

--- src/test_network.py
+++ src/test_network.py
@@ -6,7 +6,7 @@ from training_data import process_training_data, get_files, get_test_files
 # Width and Height of the board
 BOARD_SIZE = 20
 
-LEARNING_RATE = 0.001
+LEARNING_RATE = 0.1
 
 FILE_COUNT = 100
 
@@ -74,18 +74,8 @@ def network():
         for j in range (len(training_data[i])):
             batch_input = training_data[i][j][0]
             batch_output = transform_training_output_for_tf(training_data[i][j][1])
-            #print("Game: %s, Move: %s, Winner: %s" % (i + 1, j + 1, batch_output[0][0]))
             sess.run(train_step, feed_dict={input: batch_input, correct: batch_output})
 
-            """print("********************************")
-            #print("Ouput from network:")
-            #printableOuput = sess.run(model, feed_dict={input: batch_input, correct: batch_output})
-            #print(printableOuput[0][0])
-            #print("Output from training data:")
-            #print(transform_training_output_for_tf(training_data[i][j][1])[0][0])
-            #print("********************************")
-
-    """
     correct = 0
     for i in range(0, len(training_data)):
         for j in range(0, len(training_data[i])):
@@ -93,12 +83,10 @@ def network():
             batch_output = transform_training_output_for_tf(training_data[i][j][1])
             output = sess.run(model, feed_dict={input: batch_input})
             output = output[0][0]
-            print(output)
             if output < 0.5:
-                output = -1
+                output = 0
             else:
                 output = 1
-            #print(output, batch_output[0][0])
             if output == batch_output[0][0]:
                 correct += 1
 
@@ -107,70 +95,31 @@ def network():
     accuracy = (correct / size) * 100
     print("%s percent" % (accuracy))
 
-    """
-
-
-    # Determine accuracy
-    #correct_predicition = tf.equal(tf.argmax(model, 1), tf.argmax(correct, 1))
-
-    #accuracy = tf.reduce_mean(tf.cast(correct_predicition, tf.float32))
-
-    #print(sess.run(accuracy, feed_dict={input: testing_data[10][0][0], correct: testing_data[10][0][1]}))
-
-    accuracy_list = []
-
-    for i in range (0, len(testing_data)):
-        for j in range(0, len(testing_data[i])):
-            batch_input = testing_data[i][j][0]
-            batch_output = testing_data[i][j][1]
-            print(batch_output)
-            output = sess.run(model, feed_dict={input:batch_input, correct: transform_training_output_for_tf(batch_output)})
-            output = output[0][0]
-            print(output)
-            accuracy_list.append(abs(output/batch_output)*100)
-    print(accuracy_list)
-
-    
-    #accuracy_list = []
-    # For each game
-    for i in range (0, len(testing_data)):
-        # For each move in the game
-        for j in range (0, len(testing_data[i])):
-            batch_input = testing_data[i][j][0]
-            batch_output = testing_data[i][j][1]
-            printableOuput = sess.run(model, feed_dict={input: batch_input, correct: batch_output})
-            accuracy_list.append([testing_data[i][j][1], printableOuput[0][0], sess.run(accuracy, feed_dict={input: testing_data[i][j][0], correct: testing_data[i][j][1]})])
-            
-    #total_accuracy = sum(accuracy_list) / float(len(accuracy_list))
-    #print(accuracy_list)
-    #print("Neural Network Accuracy: ", total_accuracy)
-    """
-
 def transform_training_output_for_tf(actualTrainingOutput):
-	return [
-	    [actualTrainingOutput, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-	]
-
-
+    if (actualTrainingOutput == -1):
+        actualTrainingOutput = 0
+    return [
+        [actualTrainingOutput, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
 
 if __name__ == '__main__':
     network()
