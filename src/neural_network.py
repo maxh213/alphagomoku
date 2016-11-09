fixed the inputs and accuracy calculation and cross entropy tracking
Former-commit-id: 0ba49bb07d2a1dcf233930ad051bd290823b1dfc

src/neural_network.py
@@ -20,8 +20,8 @@ LEARNING_RATE = 0.1
 # The rate at which neurons are kept after learning
 KEEP_PROBABILITY = 0.5
 
-TRAINING_DATA_FILE_COUNT = 500
-TEST_DATA_FILE_COUNT = 50
+TRAINING_DATA_FILE_COUNT = 50
+TEST_DATA_FILE_COUNT = 10
 
 
 # THIS BELONGS IN training_data.py
@@ -41,8 +41,6 @@ trainingData[0][0][1] = winner of first game
 trainingData[0][1][0][0] = first line of first move of first game
 trainingData[0][1][0][0][0] = first tile on first line of first move of first game
 '''
-
-
 def get_training_data():
 	# Obtain files for processing
 	files = get_files()
@@ -55,23 +53,13 @@ def get_test_data():
 
 
 def get_weight_variable(shape):
-	return tf.Variable(tf.random_uniform(shape, -1, 1))
-
-
-# initial = tf.truncated_normal(shape, stddev=0.1)
-# return tf.Variable(initial)
+	return tf.Variable(tf.truncated_normal(shape, stddev=0.1))
 
 def get_bias_variable(shape):
-	return tf.Variable(tf.random_uniform(shape, -0.1, 0.1))
-
-
-# initial = tf.constant(0.1, shape=shape)
-# return tf.Variable(initial)
+	return tf.Variable(tf.constant(0.1, shape=shape))
 
 def conv2d(x, W):
 	# [(Batch) 1, x, y, (Channels) 1]
-	# x was 2, y was 4
-	# Changed to match size of patch / window (5, 5)
 	return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 
@@ -79,40 +67,12 @@ def avg_pool_2x2(x):
 	# A 4-D Tensor with shape [batch, height, width, channels]
 	# ksize: A list of ints that has length >= 4. The size of the window for each dimension of the input tensor.
 	# strides: A list of ints that has length >= 4. The stride of the sliding window for each dimension of the input tensor.
-
-	'''
-	TODO:We should have a [1, 2, 2, 1] ksize/stride, I set it as [1,1,1,1] before because I thought it didn't matter because we hot footed it.
-	However it gets reshaped later to 20x20.
-	'''
-	return tf.nn.max_pool(x, ksize=[1, 1, 1, 1], strides=[1, 1, 1, 1], padding='SAME')
-
-
-# even_count = -1 winner
-# odd_count = 1 winner
-def get_winner_from_output(output, print_counts):
-	odd_count = 0
-	even_count = 0
-	for i in range(len(output)):
-		if output[i] % 2 == 0:
-			even_count += 1
-		elif output[i] % 2 != 0:
-			odd_count += 1
-	if print_counts:
-		print("Chance the nn thinks it might be 1: " + str(odd_count) + "/20")
-		print("Chance the nn thinks it might be -1: " + str(even_count) + "/20")
-	if odd_count < even_count:
-		return -1
-	elif even_count < odd_count:
-		return 1
-	else:
-		# have to do something if the nn is 50:50
-		return -1
+	return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 
 def conv_network():
 	print("Convolutional Neural Network training beginning...")
-	print(
-		"If you see any 'can't read file' error messages below, please ignore them for now, this is normal behaviour in this early build")
+	print("If you see any 'can't read file' error messages below, please ignore them for now, this is normal behaviour in this early build")
 	print("------------------------------------------------")
 
 	# Get training data
@@ -120,28 +80,17 @@ def conv_network():
 	testing_data = get_test_data()
 
 	# Set up placeholders for input and output
-	training_input = tf.placeholder(tf.float32, [BOARD_SIZE * BOARD_SIZE])
-	training_output = tf.placeholder(tf.float32, [BOARD_SIZE * BOARD_SIZE])
+	training_input = tf.placeholder(tf.float32, [BOARD_SIZE, BOARD_SIZE])
+	training_output = tf.placeholder(tf.float32, [1, 100])
 
 	# Initialise weights and biases
 	# first layer
 	# looking at a 5x5 grid at each point in the board_size
 	# 1 is the number of input channels so this shouldn't change
-	conv_weights1 = get_weight_variable([5, 5, 1, 20])
+	conv_weights1 = get_weight_variable([5, 5, 1, 32])
 	# bias is always the same as the last in the shape above
-	conv_bias1 = get_bias_variable([20])
+	conv_bias1 = get_bias_variable([32])
 
-	'''
-	TODO:
-	I assumed below previously that we'd only need one colour channel but should we need more?
-	The MNIST tutorial has two colours essentially, black and white
-	but a "pixel" on our board has 3 states:
-		-person to go next has made a move there
-		-person to get after has made a move there
-		-no one has made a move there
-
-	It might help?
-	'''
 	# -1 and 1 are meant to be the colour channels of the image so no need to change them
 	# 20 by 20 is the boardsize
 	input_image = tf.reshape(training_input, [-1, 20, 20, 1])
@@ -151,93 +100,55 @@ def conv_network():
 
 	# second layer
 	# here we'll have 40 features for each 5x5 patch and 20 input channels
-	conv_weights2 = get_weight_variable([5, 5, 20, 40])
-	conv_bias2 = get_bias_variable([40])
+	conv_weights2 = get_weight_variable([5, 5, 32, 64])
+	conv_bias2 = get_bias_variable([64])
 
 	convolution2 = tf.nn.relu(conv2d(pool1, conv_weights2) + conv_bias2)
 	pool2 = avg_pool_2x2(convolution2)
 
-	fully_connected_weights1 = get_weight_variable([5 * 5 * 40, 1000])
+	fully_connected_weights1 = get_weight_variable([1600, 1000])
 	fully_connected_bias1 = get_bias_variable([1000])
 
-	'''
-	TODO:
-	--Tutorial's
-	>>> 7*7*64
-	3136
-	>>> 28**2
-	784
-	>>> 3136/784
-	4.0
-	--Our's
-	>>> 20**2
-	400
-	>>> 5*5*40
-	1000
-	>>> 1000/400
-	2.5
-
-	--Should ours match?
-	'''
-	pool2_flat = tf.reshape(pool2, [-1, 5 * 5 * 40])
+	pool2_flat = tf.reshape(pool2, [-1, 1600])
 	fully_connected_output1 = tf.nn.relu(tf.matmul(pool2_flat, fully_connected_weights1) + fully_connected_bias1)
 
-	# get rid of some neurons
 	keep_prob = tf.placeholder(tf.float32)
 	fully_connected1_drop = tf.nn.dropout(fully_connected_output1, keep_prob)
 
-	'''
-	TODO:
-	THE 25 BELOW SHOULD BE A 2 FOR US (-1, 1) BECAUSE IT'S 10 IN THE TUTORIAL (BECAUSE NUMBERS 0-9)
+	fully_connected_weights2 = get_weight_variable([1000, 100])
+	fully_connected_bias2 = get_bias_variable([100])
 
-	THE 1000 BELOW (1024 IN TUTORIAL) should match up with our 5*5 above (7*7 in tutorial):
-	>>> 1024/64
-	16.0
-	>>> 784/49
-	16.0
-	>>> 784/(7*7)
-	16.0
-	>>> 
+	tf_output = tf.nn.softmax(tf.matmul(fully_connected1_drop, fully_connected_weights2) + fully_connected_bias2, 0)
 
-	1024 is just the number of neurons though, but it's probably improtant to have the correct amount of neurons for each feautre
-
-	'''
-	fully_connected_weights2 = get_weight_variable([1000, 25])
-	fully_connected_bias2 = get_bias_variable([25])
-
-	output = tf.nn.softmax(tf.matmul(fully_connected1_drop, fully_connected_weights2) + fully_connected_bias2, 0)
-	output = tf.reshape(output, [-1])
-	# output = tf.sigmoid(output)
-
-	cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(output, training_output))
+	cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(tf_output, training_output))
 	train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
-	# correct_prediction = tf.equal(tf.argmax(output,1), tf.argmax(training_output,1))
-	# accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+	correct_prediction = tf.equal(tf.argmax(tf_output,1), tf.argmax(training_output,1))
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 	sess = tf.Session()
 	sess.run(tf.initialize_all_variables())
 
+	print("Network training starting!")
 	print_counter = 0
 	# For each game
 	for i in range(len(training_data)):
 		# For each move in the game
 		for j in range(len(training_data[i])):
-			batch_input = onehot_input(training_data[i][j][0])
-			batch_output = onehot_input(transform_training_output_for_tf(training_data[i][j][1]))
-			sess.run(train_step, feed_dict={training_input: batch_input, training_output: batch_output,
-											keep_prob: KEEP_PROBABILITY})
-			print_counter = print_counter + 1
+			batch_input = training_data[i][j][0]
+			batch_output = transform_training_output_for_tf(training_data[i][j][1])
+			entropy, _ = sess.run([cross_entropy,train_step], feed_dict={training_input: batch_input, training_output: batch_output, keep_prob: KEEP_PROBABILITY})
 			if print_counter % 1000 == 0:
-				printable_output = sess.run(output,
-											feed_dict={training_input: batch_input, training_output: batch_output,
-													   keep_prob: 1})
-				print("Game number: " + str(i))
-				print("Output from network for assumed winner (200 or over = 1, less than 200 = -1:")
-				print(sess.run(tf.argmax(printable_output, 0)))
-				print("Output from training data:")
-				print(training_data[i][j][1])
-				print("********************************")
+				print("Game Number: " + str(i))
+				print("Entropy: " + str(entropy))
+				output = sess.run(tf_output, feed_dict={training_input: batch_input, keep_prob: 1.0})
+				correct_prediction = tf.equal(tf.argmax(output,1), tf.argmax(batch_output,1))
+				accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+				print("Network output: " + str(sess.run(tf.argmax(output,1))))
+				print("Actual output: " + str(sess.run(tf.argmax(batch_output,1))))
+				print("Accuracy: " + str(sess.run(accuracy)))
+				print("***")
+			print_counter += 1
 
 	print("NN training complete, moving on to testing.")
 
@@ -247,113 +158,34 @@ def conv_network():
 	correct = 0
 	for i in range(0, len(testing_data)):
 		for j in range(0, len(testing_data[i])):
-			test_input = onehot_input(testing_data[i][j][0])
-			test_output = onehot_input(transform_training_output_for_tf(testing_data[i][j][1]))
-			comparable_output = sess.run(output, feed_dict={training_input: test_input, training_output: test_output,
-															keep_prob: 1})
-			comparable_output = sess.run(tf.argmax(comparable_output, 0))
-			if comparable_output <= 200:
-				comparable_output = -1
-			elif comparable_output > 200:
-				comparable_output = 1
-			if print_counter % 50 == 0:
-				print("output: " + str(comparable_output) + ", winner: " + str(testing_data[i][j][1]))
-			if comparable_output == testing_data[i][j][1]:
+			batch_input = testing_data[i][j][0]
+			batch_output = transform_training_output_for_tf(testing_data[i][j][1])
+			output = sess.run(tf_output, feed_dict={training_input: batch_input, keep_prob: 1.0})
+			correct_prediction = tf.equal(tf.argmax(output,1), tf.argmax(batch_output,1))
+			accuracy = sess.run(tf.reduce_mean(tf.cast(correct_prediction, "float")))
+			if accuracy < 0.5:
 				correct += 1
+
 
 	print("correct: %s" % (correct))
 	print("number: %s" % size)
-	accuracy = (correct / size) * 100
-	print("%s percent" % (accuracy))
-
-
-# This is now deprecated, we'll be editing the conv_network going forward
-def network():
-	print("Neural Network training beginning...")
-	print(
-		"If you see any 'can't read file' error messages below, please ignore them for now, this is normal behaviour in this early build")
-	print("------------------------------------------------")
-
-	# Get training data
-	training_data = get_training_data()
-	testing_data = get_test_data()
-
-	# Set up placeholders for input and output
-	input = tf.placeholder(tf.float32, [BOARD_SIZE, BOARD_SIZE])
-	correct = tf.placeholder(tf.float32, [BOARD_SIZE, BOARD_SIZE])
-
-	# Initialise weights and biases
-	weights = get_weight_variable([BOARD_SIZE, BOARD_SIZE])
-	bias = get_bias_variable([BOARD_SIZE, BOARD_SIZE])
-
-	# Define the model
-	# the 0 argument at the end is the index you wish to apply softmax to
-	# if you don't specify it assumes -1 which means the last entry
-	model = tf.nn.softmax(tf.matmul(input, weights) + bias, 0)
-	model = tf.sigmoid(model)
-
-	# Define Cross Entropy function
-	cross_entropy = tf.reduce_mean(-tf.reduce_sum(correct * tf.log(model)))
-
-	# Define a training step
-	train_step = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(cross_entropy)
-
-	# Prepare model for training
-	init = tf.initialize_all_variables()
-
-	sess = tf.Session()
-	sess.run(init)
-
-	# For each game
-	for i in range(len(training_data)):
-		# For each move in the game
-		for j in range(len(training_data[i])):
-			batch_input = training_data[i][j][0]
-			batch_output = transform_training_output_for_tf(training_data[i][j][1])
-			sess.run(train_step, feed_dict={input: batch_input, correct: batch_output})
-			print("********************************")
-			print("Output from network:")
-			printable_output = sess.run(model, feed_dict={input: batch_input, correct: batch_output})
-			print(printable_output[0])
-			print("Output from training data:")
-			print(transform_training_output_for_tf(training_data[i][j][1])[0][0])
-			print("********************************")
-
-
-# Tensorflow cannot output a different shape than its input (which is a 20x20 board)
-# so we have to do this so we can compare the actual training ouput with the tensorflow output
-'''
-	I've altered this method so that the bottom 200 tiles are filled to signify player 1 winning
-	and the top 200 rows filled to signify -1 winning.
-
-	This is done so we can compare out output with argmax in tensorflow. 
-	Argmax will return the max index in the 0 dimension (which is the only dimension since our input is hotfooted).
-	If this index is 200 or after it means it thinks player 1 will win if it's below 200 then player -1
-
-	^^^This doesn't actually work and neither does filling the first and second element to represent the winner 
-	(because most the time they're 0 but the output data seems to just have random elements in the 400 long array flash on and off
-	 ...but most of the elements are 0)
-
-	 TODO: we need to think of a stratgy for making our output training data meaningful to our tensorflow program.
-	 It'll probably be smart to talk to Radu about this.
-'''
+	percentage = (correct / size) * 100
+	print("%s percent" % (percentage))
 
 
 def transform_training_output_for_tf(actual_training_output):
+	if actual_training_output == -1:
+		actual_training_output = 0
 	output = []
 	count = 0
-	for i in range(BOARD_SIZE):
+	for i in range(1):
 		output.append([])
-		for j in range(BOARD_SIZE):
-			count += 1
-			if count > 200 and actual_training_output == 1:
-				output[i].append(1)
-			elif count <= 200 and actual_training_output == -1:
-				output[i].append(1)
+		for j in range(100):
+			if j == actual_training_output:
+				output[i].append(100)
 			else:
 				output[i].append(0)
 	return output
-
 
 
 def onehot_input(input):