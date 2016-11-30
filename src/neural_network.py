2e7b2edd884979a902aef52f8d42ffb665a19b75
Max Harris <mh568@kent.ac.uk>
2016-11-30 23:02:26 +0000

    Wrote a function to shuffle the inputs and outputs every epoche
    Former-commit-id: 0a5ae3af42a84dec51e50d9877c4a39bb060cde5

--- src/neural_network.py
+++ src/neural_network.py
@@ -1,16 +1,16 @@
 import tensorflow as tf
 import math
-from random import randrange
+import random
 from training_data import get_training_data, get_test_data
 from board import BOARD_SIZE
 
-TRAINING_ITERATIONS = 10
+TRAINING_ITERATIONS = 5000
 DEBUG_PRINT_SIZE = 5
 
 LEARNING_RATE = 0.003
 #The below is only needed for gradient decent
-#DECAY_LEARNING_RATE_EVERY_STEP_AMOUNT = math.ceil(TRAINING_ITERATIONS/4)
-#LEARNING_RATE_DECAY_AMOUNT = 0.1
+#DECAY_LEARNING_RATE_EVERY_N = math.ceil(TRAINING_ITERATIONS/4)
+#DECAY_RATE = 0.96
 
 # The rate at which neurons are kept after learning
 KEEP_SOME_PROBABILITY = 0.7
@@ -26,8 +26,8 @@ GRAPH_LOGS_SAVE_FILE_PATH = "save_data/logs/"
 INPUT_SIZE = BOARD_SIZE ** 2
 OUTPUT_SIZE = 2
 #You can change the below to be whatever you want, the higher they are the longer it'll take to run though
-LAYER_1_WEIGHTS_SIZE = 400
-LAYER_2_WEIGHTS_SIZE = 200
+LAYER_1_WEIGHTS_SIZE = 600
+LAYER_2_WEIGHTS_SIZE = 400
 
 
 def get_weight_variable(shape):
@@ -58,8 +58,8 @@ def convert_training_to_batch(training_data):
 	train_output = []
 	for i in range(len(training_data)):
 		for j in range(len(training_data[i])):
-			# if the move is less than 15 and the game lasts more than 15 moves don't bother
-			if not (j < 15 and len(training_data[i]) > 15):
+			# if the move number is less than 5 and the game lasts more than 5 moves don't bother
+			if not (j < 5 and len(training_data[i]) > 5):
 				train_input.append(training_data[i][j][0])
 				# If training_data[i][j][1] == -1 then an argmax function would identify the first index 0 as the highest
 				# If training_data[i][j][1] == 1 then the argmax function would identify index 1 as the highest
@@ -77,7 +77,6 @@ def neural_network_train(should_use_save_data):
 	testing_data = get_test_data(TEST_DATA_FILE_COUNT)
 	print("Training data loaded!")
 
-	# Set up placeholders for input and output
 	training_input = tf.placeholder(tf.float32, [None, INPUT_SIZE])
 	training_output = tf.placeholder(tf.float32, [None, OUTPUT_SIZE])
 	keep_prob = tf.placeholder(tf.float32)
@@ -91,7 +90,6 @@ def neural_network_train(should_use_save_data):
 
 	layer_1_output = tf.nn.tanh(tf.matmul(training_input, layer_1_weights) + layer_1_bias)
 	
-	#[2, 2] not [400, 2] because the layer above transforms the output
 	layer_2_weights = get_weight_variable([LAYER_1_WEIGHTS_SIZE, LAYER_2_WEIGHTS_SIZE])
 	layer_2_bias = get_bias_variable([LAYER_2_WEIGHTS_SIZE])
 
@@ -116,10 +114,9 @@ def neural_network_train(should_use_save_data):
 	cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(tf_output, training_output))
 	tf.histogram_summary("cross_entropy", cross_entropy)
 
-	#learning_rate = tf.train.exponential_decay(LEARNING_RATE, global_step, DECAY_LEARNING_RATE_EVERY_STEP_AMOUNT, LEARNING_RATE_DECAY_AMOUNT, staircase=True)
+	#learning_rate = tf.train.exponential_decay(LEARNING_RATE, global_step, DECAY_LEARNING_RATE_EVERY_N, DECAY_RATE, staircase=True)
 	#train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy, global_step=global_step)
 	train_step = tf.train.AdamOptimizer(LEARNING_RATE).minimize(cross_entropy, global_step=global_step)
-	#train_step = tf.train.AdadeltaOptimizer(LEARNING_RATE, rho=LEARNING_RATE_DECAY_AMOUNT, epsilon=EPSILON, use_locking=False).minimize(cross_entropy, global_step=global_step)
 
 	correct_prediction = tf.equal(tf.argmax(tf.nn.softmax(tf_output),1), tf.argmax(training_output,1))
 	
@@ -140,11 +137,11 @@ def neural_network_train(should_use_save_data):
 		#Try load the weights and biases from when the network was last run
 		try:
 			saver.restore(sess, MODEL_SAVE_FILE_PATH)
-			print("TensorFlow model restored from last session.")
+			print("TensorFlow model loaded from last session.")
 		except ValueError as error:
 			print("Could not load a TensorFlow model from the last session because none was found.")
 	else:
-		print("Did not load previous save data because you did not pass in a boolean flag saying True. If you wish to load the previous save data run: python3 main.py True")
+		print("Previous save data not loaded! If you wish to load the previous save data run: python3 main.py True")
 
 	print("Network training starting!")
 
@@ -152,16 +149,22 @@ def neural_network_train(should_use_save_data):
 	train_input_batch = one_hot_input_batch(train_input_batch)
 	feed_dict_train={training_input: train_input_batch, training_output: train_output_batch, keep_prob: KEEP_SOME_PROBABILITY}
 	feed_dict_train_keep_all={training_input: train_input_batch, training_output: train_output_batch, keep_prob: KEEP_ALL_PROBABILITY}
-
+	test_input_batch, test_output_batch = convert_training_to_batch(testing_data)
+	test_input_batch = one_hot_input_batch(test_input_batch)
+	feed_dict_test={training_input: test_input_batch, training_output: test_output_batch, keep_prob: KEEP_ALL_PROBABILITY}
 	
 	for i in range(TRAINING_ITERATIONS):
-		#TODO: IT'S A GOOD IDEA TO SHUFFLE THE TRAINING DATA AFTER EVERY EPOCHE TO AVOID BAIS
+		print("-")
 		print("Training step: " + str(sess.run(global_step)) + "/" + str(TRAINING_ITERATIONS-1))
 		entropy, _, train_step_accuracy = sess.run([cross_entropy,train_step, accuracy], feed_dict=feed_dict_train)
 		print("Entropy: " + str(entropy))
 		print("Training Step Result Accuracy: " + str(train_step_accuracy))
-	print("Training Accuracy: " + str(sess.run(accuracy, feed_dict=feed_dict_train_keep_all)))
-	
+		if i % 100 == 0:
+			print("Testing Accuracy if training were to finish now: " + str(sess.run(accuracy, feed_dict=feed_dict_test)))
+		train_input_batch, train_output_batch = shuffle_batch_inputs_and_outputs(train_input_batch, train_output_batch)
+		feed_dict_train={training_input: train_input_batch, training_output: train_output_batch, keep_prob: KEEP_SOME_PROBABILITY}
+
+
 	debug_outputs = sess.run(tf_output, feed_dict=feed_dict_train_keep_all)
 	print_debug_outputs(DEBUG_PRINT_SIZE, train_output_batch, debug_outputs)
 
@@ -172,15 +175,9 @@ def neural_network_train(should_use_save_data):
 	save_path = saver.save(sess, MODEL_SAVE_FILE_PATH)
 	print("TensorFlow model saved in file: %s" % save_path)
 
-	test_input_batch, test_output_batch = convert_training_to_batch(testing_data)
-	test_input_batch = one_hot_input_batch(test_input_batch)
-	feed_dict_test={training_input: test_input_batch, training_output: test_output_batch, keep_prob: KEEP_ALL_PROBABILITY}
-	print("Testing Accuracy: " + str(sess.run(accuracy, feed_dict=feed_dict_test)))
-
 	training_accuracy = sess.run(accuracy, feed_dict=feed_dict_train_keep_all)
-	testing_accuracy = sess.run(accuracy, feed_dict=feed_dict_test))
+	testing_accuracy = sess.run(accuracy, feed_dict=feed_dict_test)
 	print_accuracy_percentage(training_accuracy, testing_accuracy)
-
 
 	print("Run the following command to see the graphs produced from this training:")
 	print("tensorboard --logdir=" + GRAPH_LOGS_SAVE_FILE_PATH)
@@ -198,8 +195,17 @@ def print_debug_outputs(amount, train_output_batch, debug_outputs):
 
 def print_accuracy_percentage(training_accuracy, testing_accuracy):
 	training_accuracy = "%.2f" % (training_accuracy * 100)
-	testing_accuracy = "%.2f" % (testing_accuracy * 100
+	testing_accuracy = "%.2f" % (testing_accuracy * 100)
 	print("-----")
 	print("Training Accuracy: " + str(training_accuracy) + "%")
 	print("Testing Accuracy: " + str(testing_accuracy) + "%")
 	print("-----")
+
+def shuffle_batch_inputs_and_outputs(batch_input, batch_ouput):
+	#combine the lists (so they keep the same shuffle order), shuffle them, then split them
+	#zipping will make the two lists [a, b] and [1, 2] = [(a, 1), (b, 2)]
+	combined_batch = list(zip(batch_input, batch_ouput))
+	random.shuffle(combined_batch)
+	#[:] just essentially casts the tuple result from zip into the same list variables we already used
+	batch_input[:], batch_ouput[:] = zip(*combined_batch)
+	return batch_input, batch_ouput
