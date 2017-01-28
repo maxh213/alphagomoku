f7742fd608a1492f02644b136dc58d6941626ead
Harry Clarke <harry-clarke@outlook.com>
2017-01-28 12:23:29 +0000

    Removed unnnecessary deep copy of board from training_data; the board is already a copy.
    # Conflicts:
    #	src/training_data.py
    
    
    Former-commit-id: 227a27c408aabd2e281b334a14d8b3e4845a4368

--- src/training_data.py
+++ src/training_data.py
@@ -1,23 +1,14 @@
 #! /usr/bin/env python3
 import glob
-from copy import deepcopy
 from sys import argv
 
 from typing import List, Tuple
-from board import Board, BoardStruct
+from board import Board, BoardStruct, MovesStruct
 
 """
 Training data is represented as a list of moves/boards, and the winner for that game.
 """
 TrainingDataStruct = Tuple[List[BoardStruct], int]
-
-
-"""
-Moves are represented as a tuple of x and y coords.
-"""
-MoveStruct = Tuple[int, int]
-
-MovesStruct = List[MoveStruct]
 
 
 def parse_training_file(path: str) -> MovesStruct:
@@ -32,29 +23,29 @@ def parse_training_file(path: str) -> MovesStruct:
 
 def simulate(moves: MovesStruct, should_print: bool=False) -> TrainingDataStruct:
 	board = Board()
-	all_boards = [deepcopy(board.board)]
+	all_boards = [board.get_board()]
 	p = -1
 	for x, y in moves:
-		assert board.board[x][y] == 0
-		board.board[x][y] = p
-		all_boards.append(deepcopy(board.board))
+		assert board.move(x, y, p)
+		all_boards.append(board.get_board())
 		if should_print:
 			board.print_board()
-		winner = board.decide_winner()
+		winner, _ = board.decide_winner()
 		if winner != 0:
 			return all_boards, winner
 		p = -p
 	raise ValueError('Winner still not determined after all moves have been made.')
 
 
-def process_training_data(paths: List[str]):
+def process_training_data(paths: List[str], should_print=False):
 	training_data = []
 	for path in paths:
 		path_data = []
-		print('processing file', path)
+		if should_print:
+			print('processing file', path)
 		moves = parse_training_file(path)
 		try:
-			boards, winner = simulate(moves, should_print=False)
+			boards, winner = simulate(moves, should_print=should_print)
 			path_data.extend((b, winner) for b in boards)
 		except ValueError as error:
 			print ("Caught the following error for file: ", path, " Error: ", error)
@@ -77,4 +68,7 @@ def get_test_files() -> List[str]:
 	return glob.glob("../resources/training/freestyle/freestyle2/*.psq")[500:]
 
 if __name__ == '__main__':
-	process_training_data(argv[1:])
+	if len(argv) > 1:
+		process_training_data(argv[1:], should_print=True)
+	else:
+		process_training_data(get_files(), should_print=True)
