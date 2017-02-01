92c73305a392b325e48b0e56177d6611a97a2280
Matthew Boakes <mb689@kent.ac.uk>
2017-02-01 15:11:48 +0000

    Cleaned up main.py
    Former-commit-id: 8de7aa662680f833300d97780d4dcfcc10d10228
    Former-commit-id: e588e2b77540d06f62228df57e052b257c9dfff2

--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
+from sys import argv
 from typing import Tuple
 
 from neural_network import neural_network_train
-from sys import argv
 
 ShouldSaveStruct = bool
 ArgsStruct = Tuple[ShouldSaveStruct]
 
 
-def parse_args(args: str) -> ArgsStruct:
-	'''
+def parse_args(args: str) -> bool:
+	"""
 	Responsible for parsing args into the form of the ArgsStruct.
-	'''
+	"""
 	should_use_save = False
 	for arg in args:
 		if arg.lower() == 'true':
@@ -22,6 +22,7 @@ def main():
 	if __name__ == '__main__':
 		should_use_save_data = parse_args(argv[1:])
 		neural_network_train(should_use_save_data)
+
 
 if __name__ == "__main__":
 	main()
