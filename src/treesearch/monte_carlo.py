1072a684889c10c22467e8ae3ee2868baf6a7707
Max Harris <mh568@kent.ac.uk>
2017-02-05 16:56:17 +0000

    updates

--- src/treesearch/monte_carlo.py
+++ src/treesearch/monte_carlo.py
@@ -1,10 +1,10 @@
 from datetime import datetime, timedelta
 
 from gomokuapp.board import Board, BoardStruct, MoveStruct
-from gomokuapp.neuralnetwork import setup_network, use_network
+from neuralnetwork.neural_network import setup_network, use_network
 
-class _Network:
-	
+class Neural_Network:
+
 	def __init__(self):
 		self.training_input, self.heuristic, self.keep_prob, self.tf_output, self.sess = setup_network()
 
@@ -19,15 +19,14 @@ class Node:
 	Represents a move that can be made, how good that move is, and what moves can be made after it.
 	"""
 
-	def __init__(self, move: MoveStruct, board: Board, net: _Network = None):
+	def __init__(self, move: MoveStruct, board: Board, neural_network: Neural_Network):
 		self.children = []
 		self.x, self.y = move
 		self._board = board
-		if net is None:
-			net = _Network()
+		self.neural_network = neural_network
 
 		# Value between -1 and 1, where 1 means we've won, and -1 means we've lost.
-		self.value = 1 if board.decide_winner() is not None else net.nn(board)
+		self.value = 1 if board.decide_winner() is not None else self.neural_network.nn(board)
 
 	def get_value(self) -> int:
 		return self.value
@@ -44,7 +43,7 @@ class Node:
 		for x, y in moves:
 			valid = self._board.move(x, y, p)
 			assert valid
-			child = Node((x, y), self._board)
+			child = Node((x, y), self._board, self.neural_network)
 			self.children.append(child)
 			reversed_move = self._board.reverse_move()
 			assert reversed_move == (x, y, p), "%r vs %r" % (reversed_move, (x, y, p))
@@ -78,6 +77,7 @@ class MonteCarlo:
 		self.states = []
 		self.wins = {}
 		self.plays = {}
+		self.neural_network = Neural_Network()
 
 	def update(self, state: BoardStruct) -> None:
 		self.states.append(state)
@@ -88,8 +88,8 @@ class MonteCarlo:
 			self.run_simulation()
 
 	def run_simulation(self):
-		states_copy = self.states[:]
-		state = states_copy[-1]
+		states_copy = self.states[:] #{:] creates a copy of the list
+		state = states_copy[-1] #gets the last element in the states_copy list
 
 		for _ in range(self.max_moves):
 			legal = self.board.get_possible_moves()
@@ -97,11 +97,25 @@ class MonteCarlo:
 			assert self.board.move(x, y, self.board.get_next_player())
 			state = self.board.get_board()
 			states_copy.append(state)
-
-			winner, _ = self.board.decide_winner()
-
-			if winner is not 0:
+			#Change the below to _winner instead of decide_winner cause we want 0 if no winner not none
+			winner = self.board._winner
+			if winner is not 0: #0 means there is no winner -1, or 1 are the possible winners
 				break
 
-	def choice(self):
-		pass
+	def choice(self, legal_moves):
+		'''
+			TODO:
+			-Add depth
+			-Keep history of played moves for future use
+			-Use UCB1 to pick branches of the tree when there is enough play data on them.
+			-This method
+		'''
+		print(legal_moves)
+		return 1, 1
+
+
+def main():
+	board = Board()
+	monte_carlo = MonteCarlo(board)
+	monte_carlo.update(BoardStruct)
+	monte_carlo.get_play()
