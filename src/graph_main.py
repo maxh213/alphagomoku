6f63b68abd3d940f73cec7cd0feb2853be64f0fc
jamie <jp486@kent.ac.uk>
2017-02-22 18:14:19 +0000

    Added heatmap saving

--- src/graph_main.py
+++ src/graph_main.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
 import numpy as np
 import numpy.random
 
@@ -14,16 +16,27 @@ FILE_LOCATION = "resources/training/training_data_max/2x1-31(98).psq"
 
 # Generate network input, determine network output, create heatmap
 def main():
+	# True: Save heatmap to PDF
+	# False: Do not save heatmap to PDF
+	should_save = True
+
 	network = Neural_Network()
+
 	board = parse_training_file(FILE_LOCATION)
 	inp = simulate(board)
-	# Random board
-	instance = inp[0][30]
+	instance = inp[0][30] # Some move within a game
 	b = Board(instance)
+
+	# Gather neural network outputs for each possible move on board
 	outputs = gather_outputs(b, network)
 	b.print_board()
 	draw_graph(outputs)
 
+	if should_save:
+		save_to_PDF()
+
+def save_to_PDF():
+	print("hello")
 
 # For a given board, determine the network's output for all adjacent states
 def gather_outputs(board, network):
@@ -58,9 +71,15 @@ def draw_graph(outputs):
 
 	heatmap, _, _ = np.histogram2d(x, y, weights=outputs, bins = 20)
 
+	f = plt.figure()
+
 	plt.clf()
+	plt.title("Heatmap of Neural Network Output")
 	plt.imshow(heatmap)
 	plt.show()
+
+	f.savefig("resources/heatmaps/heatmap.pdf")
+
 
 if __name__ == "__main__":
     main()
