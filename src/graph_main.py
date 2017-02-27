b94bd808ab665b48d5fae394995ea0b0922170e9
jamie <jp486@kent.ac.uk>
2017-02-27 16:23:44 +0000

    User can now generate graph for any game using command line arguments

--- src/graph_main.py
+++ src/graph_main.py
@@ -10,17 +10,17 @@ from treesearch.monte_carlo import Neural_Network
 
 from copy import deepcopy
 
-# Change as appropriate
-# TO DO: Parameterise
+import sys
+
 FILE_LOCATION = "resources/training/training_data_max/2x1-31(98).psq"
 
 # Generate network input, determine network output, create heatmap
-def main():
+def main(file_path=FILE_LOCATION, game_number=16):
 	network = Neural_Network()
 
-	board = parse_training_file(FILE_LOCATION)
+	board = parse_training_file(file_path)
 	inp = simulate(board)
-	instance = inp[0][16] # Some move within a game
+	instance = inp[0][int(game_number)] # Some move within a game
 	b = Board(instance)
 
 	# Gather neural network outputs for each possible move on board
@@ -87,4 +87,17 @@ def draw_graph(outputs, should_save=True):
 
 
 if __name__ == "__main__":
-    main()
+	# The user can supply a file location and game count to generate a heatmap for
+	# Otherwise, default values will be used
+	if len(sys.argv) == 3:
+		path = sys.argv[1]
+		game = sys.argv[2]
+		try:
+			main(path, game)
+		except IOError as fnf:
+			print("%s is not a valid file location." % (path))
+		except Exception as e:
+			print("Please enter a valid game count.")
+			print(e)
+	else:
+		main()
