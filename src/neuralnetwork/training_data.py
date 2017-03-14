98f935ea3b2d88eeda0bf185089dab8d0edec7ab
Max Harris <mh568@kent.ac.uk>
2017-03-14 18:28:25 +0000

    added more training data to the google drive

--- src/neuralnetwork/training_data.py
+++ src/neuralnetwork/training_data.py
@@ -17,6 +17,7 @@ _TRAINING_DATA_FILES += glob.glob("../resources/training/even_more_fast_training
 _TRAINING_DATA_FILES += glob.glob("../resources/training/training_data_max/*.psq")[:33000]
 _TRAINING_DATA_FILES += glob.glob("../resources/training/training_data_harry/*.psq")[-27000:]
 _TRAINING_DATA_FILES += glob.glob("../resources/training/training_data_mcmc/*.pqs")
+_TRAINING_DATA_FILES += glob.glob("../resources/training/training_data_march/*.pqs")
 
 _TEST_DATA_FILES = glob.glob("../resources/training/fast_training_data/*.psq")[-2500:]
 _TEST_DATA_FILES += glob.glob("../resources/training/more_fast_training_data/*.psq")[-1400:]
